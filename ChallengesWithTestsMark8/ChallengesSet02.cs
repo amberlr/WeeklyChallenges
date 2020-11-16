f9d6a1ed51c2c55febba9c92f7c94e19d55341c2
Amber Riley <amberlr1992@gmail.com>
2020-11-16 08:02:03 -0800

    Week 2 Challenges Complete

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +10,115 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c); //all I had to do to pass isaletter and isnotaletter
+                                     //char.IsLetter(); is a method I can use to inidicate whether the letter is actually a letter
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if(vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return (number % 2 == 0) ? true : false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return (num % 2 != 0) ? true : false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if(numbers is null || numbers.Count() == 0) //should always do null check first. you can do is or ==
+            {
+                return 0;
+            }
+            return numbers.Min() + numbers.Max();
+
+            //IEnumerable means it is a collection
+            //Could be an array or a list
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.Length < str2.Length ? str1.Length : str2.Length; //we can do the two below instead
+            //var list = new List<int> { str1.Length, str2.Length };
+            //return list.Min();
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            //int sum = numbers.Sum();
+            if(numbers == null) //null did not work with the line above it.. why?
+            {
+                return 0;
+            }
+            return numbers.Sum(); //did this instead of using sum which i initialized at the top
+            
+            ////if you don't want to use linq - try below foreach
+            //int sum = 0;
+            //foreach(var num in numbers) 
+            //{
+            //    sum += sum;
+            //}
+            //return sum;
+
+            ////if you want to do for loop
+            //for(int i = 0; i < numbers.Length; i++) 
+            //{
+            //    sum += numbers[i];
+            //}
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) //so my issue was that I had this portion in the for loop.. and I also had to put this up top
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum = sum + numbers[i];
+                }
+            } return sum;
+
+            ////if you want to do linq.. include below with nullcheck
+            //return numbers.Where(num => num % 2 == 0).Sum();
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            if (numbers.Sum() % 2 != 0) //can do number.Sum here instead of making a variable int sum = numbers.Sum();
+            {
+                return true;
+            }
+            return false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            //if(number < 0) 
+            //{
+            //    return 0;
+            //}
+            //return number / 2;
+
+            return number < 0 ? 0 : number / 2;
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,7 +8,14 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false) //I didn't have an == to false which was causing it to fail
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
@@ -18,26 +26,39 @@ namespace ChallengesWithTestsMark8
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
             throw new NotImplementedException();
+            //bool containsUpper = password.Any(char.IsUpper);
+            //bool containsLower = password.Any(char.IsLower);
+            //bool containsNumber = password.Any(char.IsDigit);
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return char.Parse(val.Substring(0, 1)); //since it wants the char we have to do char.Parse first
+                                                    //we can use the substring method then provide index and length (I think?)
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return char.Parse(val.Substring(val.Length - 1)); //have to do -1 on length so it just grabs the last character
+                                                              //in random string value
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(divisor == 0) //had to do this to include divisor being 0
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
             throw new NotImplementedException();
+            //array.Length - 1
+            //array.Length(0, 1)
+
+            
         }
 
         public int[] GetOddsBelow100()
@@ -47,7 +68,11 @@ namespace ChallengesWithTestsMark8
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++) //easier way to do this?
+            {
+                words[i] = words[i].ToUpper();
+            }
+            //did a for loop that would go thru all elements in the string and uppercase them
         }
     }
 }
