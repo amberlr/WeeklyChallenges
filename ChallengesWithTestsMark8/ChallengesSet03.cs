d0701c021462abb024e0631b4a5bf131535ab5ff
Amber Riley <amberlr1992@gmail.com>
2020-11-16 14:45:28 -0800

    Week 3 Challenges Complete

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -14,33 +14,53 @@ namespace ChallengesWithTestsMark8
                 {
                     return true;
                 }
+                //is there a way to do ternary for this if statement?
             }
             return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Count() == 0) 
+            {
+                return false;
+            }
+            //if(numbers.Sum() % 2 != 0) 
+            //{
+            //    return false;
+            //}
+            //return numbers.Sum() % 2 == 0;
+
+            return numbers.Sum() % 2 == 0 ? false : true; //can do ternary instead
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
-            //bool containsUpper = password.Any(char.IsUpper);
-            //bool containsLower = password.Any(char.IsLower);
-            //bool containsNumber = password.Any(char.IsDigit);
+            //created a bool variable to for upper case, lower case, and digit
+            bool containsUpper = password.Any(char.IsUpper);
+            bool containsLower = password.Any(char.IsLower);
+            bool containsNumber = password.Any(char.IsDigit);
+
+            //if statement that if you want true.. you need to include all of the variables I created above
+            if(containsUpper && containsLower && containsNumber) 
+            {
+                return true;
+            }
+            return false;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            return char.Parse(val.Substring(0, 1)); //since it wants the char we have to do char.Parse first
-                                                    //we can use the substring method then provide index and length (I think?)
+            //do char.Parse so it looks at the character in the string.. 
+            //0 would be the starting point (beginning of string)
+            //the 1 would be the first character in the string
+            return char.Parse(val.Substring(0, 1));
         }
 
         public char GetLastLetterOfString(string val)
         {
-            return char.Parse(val.Substring(val.Length - 1)); //have to do -1 on length so it just grabs the last character
-                                                              //in random string value
+            //did .Length - 1 because I only want the last character of the entirety of the string
+            return char.Parse(val.Substring(val.Length - 1));
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
@@ -54,16 +74,30 @@ namespace ChallengesWithTestsMark8
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
-            //array.Length - 1
-            //array.Length(0, 1)
-
-            
+            return nums.Last() - nums.First();
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            //to get odds: number % 2 != 0
+            //need to get values below 100 - so number < 100
+            //could I do a number / 2 to technically get the odds like I could evens? or do I need to do / 2 + 1 since it doesn't do decimal?
+            //50 odd numbers under 100 so technically I can just divide by 2 from 100?
+
+            //is there a way to do this with random numbers in array although the unit test does not require it?
+            int[] numbers = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25,
+                27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49,
+                51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73,
+                75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99 };
+            foreach(int num in numbers) 
+            {
+                if (num % 2 != 0) 
+                {
+                    return numbers;
+                }
+            }
+            return numbers;
+            //not sure how to convert this into linq
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
